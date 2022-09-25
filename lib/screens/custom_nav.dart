cbef430aa8b0149831c7ed9e307f4832d20b76fc
DANIEL-DEMELASH <danieldemelash5@gmail.com>
2022-09-25 18:20:00 +0300

    fix custom nav error

--- lib/screens/custom_nav.dart
+++ lib/screens/custom_nav.dart
@@ -1,6 +1,7 @@
 import 'package:container_tab_indicator/container_tab_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:telegram_ui/constants.dart';
+import 'package:telegram_ui/widgets/custom_tab.dart';
 
 class CustomNav extends StatelessWidget {
   const CustomNav({super.key});
@@ -38,23 +39,23 @@ class CustomNav extends StatelessWidget {
                 borderColor: Colors.white,
               ),
               tabs: [
-                _CustomTab(
+                CustomTab(
                   label: 'All',
                   num: 28,
                 ),
-                _CustomTab(
+                CustomTab(
                   label: 'Private',
                   num: 6,
                 ),
-                _CustomTab(
+                CustomTab(
                   label: 'Groups',
                   num: 8,
                 ),
-                _CustomTab(
+                CustomTab(
                   label: 'Channels',
                   num: 28,
                 ),
-                _CustomTab(
+                CustomTab(
                   label: 'Bots',
                   num: 1,
                 ),
@@ -77,47 +78,6 @@ class CustomNav extends StatelessWidget {
             Scaffold(
               backgroundColor: Colors.grey[200],
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _CustomTab extends StatelessWidget {
-  const _CustomTab({
-    Key? key,
-    required this.label,
-    required this.num,
-  }) : super(key: key);
-
-  final String label;
-  final int num;
-
-  @override
-  Widget build(BuildContext context) {
-    return Tab(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 10.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(label),
-            const SizedBox(
-              width: 2,
-            ),
-            Container(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.0),
-              decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
-              child: Text(
-                '$num',
-                style: const TextStyle(color: telegramBlue, fontSize: 12.0),
-              ),
-            ),
-            // )
           ],
         ),
       ),
---
+++ lib/widgets/custom_tab.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:telegram_ui/constants.dart';
+
+class CustomTab extends StatelessWidget {
+  const CustomTab({
+    Key? key,
+    required this.label,
+    required this.num,
+  }) : super(key: key);
+
+  final String label;
+  final int num;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(label),
+            const SizedBox(
+              width: 2,
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.0),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(15.0))),
+              child: Text(
+                '$num',
+                style: const TextStyle(color: telegramBlue, fontSize: 12.0),
+              ),
+            ),
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
